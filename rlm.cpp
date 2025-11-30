f3ab625f6e408ada53f3986a42e7deb76b203a1a
pandamusrex <pandamusrex@gmail.com>
2025-11-30 10:35:23 -0800

    Enable the link, print the tempo and peer count

--- rlm.cpp
+++ rlm.cpp
@@ -1,7 +1,38 @@
+#include "unistd.h"
+#include <atomic>
+#include "signal.h"
+
 #include "ableton/Link.hpp"
 
+std::atomic_bool signalled = false;
+
+static void InterruptHandler(int signo) {
+	signalled = true;
+}
+
 int main(int argc, char **argv) {
+
+	signal(SIGTERM, InterruptHandler);
+	signal(SIGINT, InterruptHandler);
+	signal(SIGHUP, InterruptHandler);
+
 	ableton::Link link(120.);
+	link.enable(true);
+
+	int numPeers = 0;
+	float sessionTempo = 120.;
+
+	do {
+		numPeers = link.numPeers();
+		sessionTempo = link.captureAppSessionState().tempo();
+		std::cout << "Num Peers: " << numPeers << " ... Tempo: " << sessionTempo << "    " <<  "\r";
+		if (signalled) {
+			break;
+		}
+		usleep(10000);
+	} while (true);
+
+	link.enable(false);
 
 	return 0;
 }
